6da108c71a4f80cfa3b0b973856af415591dece2
elliottwuTW <elliott10009@gmail.com>
2020-10-04 14:58:17 +0800

    feat: Add MongoDB and do database seeding
    Add Mongoose to use MongoDB
    Define a restaurant schema
    Create restaurantSeeder
    Render the main page (index.hbs)

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 // Include the modules and files needed in Node.js
 const express = require('express')
 const exphbs = require('express-handlebars')
+const mongoose = require('mongoose')
+const restaurant = require('./models/restaurant')
+
+const Restaurant = require('./models/restaurant')
 
 // Create the Express app
 const app = express()
 const port = 3000
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost/restaurant-list', { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongoDB error!')
+})
+db.once('open', () => {
+  console.log('mongoDB connected!')
+})
 
 // Set the template engine
 app.engine('hbs', exphbs({
@@ -17,8 +32,11 @@ app.set('view engine', 'hbs')
 app.use(express.static('public'))
 
 // Set GET routing
-app.get('/', (req, res) => {
-  res.render('index')
+app.get('/', (req, res) => { // Main page
+  Restaurant.find()
+    .lean()
+    .then(restaurants => res.render('index', { restaurants }))
+    .catch(err => console.error(err))
 })
 
 // Start and listen to the server
