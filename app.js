59be80dde1263d3fa82a131c13095e11f6dc3f1b
elliottwuTW <elliott10009@gmail.com>
2020-10-04 13:57:20 +0800

    feat: add exphbs template engine and the main page layout

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 // Include the modules and files needed in Node.js
 const express = require('express')
+const exphbs = require('express-handlebars')
 
 // Create the Express app
 const app = express()
 const port = 3000
 
+// Set the template engine
+app.engine('hbs', exphbs({
+  defaultLayout: 'layout',
+  extname: '.hbs'
+}))
+app.set('view engine', 'hbs')
+
+// Set the static files
+app.use(express.static('public'))
+
 // Set GET routing
 app.get('/', (req, res) => {
-  res.send('project init')
+  res.render('index')
 })
 
 // Start and listen to the server
